da4de6cb812996f7881ebb35d9f965675fd1abb2
Trevor Svec <tsvec5678@gmail.com>
2016-12-21 23:04:39 -0500

    update-trevor
    Basic skeleton code for a simple button and a quit button.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
 public class Main {
@@ -9,10 +11,84 @@ public class Main {
 		//testing update from local eclipse edit - Trevor
 		System.out.println("hello world");
 		
+		/*
 		ChessFrame cFrame = new ChessFrame();
 		cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		cFrame.setSize(300, 300);
 		cFrame.setVisible(true);
+		*/
+		
+		//simple button example, uncomment to test
+		/*
+		EventQueue.invokeLater(() -> {
+            SimpleEx ex = new SimpleEx();
+            ex.setVisible(true);
+        });		
+        */
+		
+		//quit button example, uncomment to test
+		EventQueue.invokeLater(() -> {
+            QuitButtonEx ex = new QuitButtonEx();
+            ex.setVisible(true);
+        });
 	}
+}
 
+
+class SimpleEx extends JFrame {
+
+    public SimpleEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+        
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
+
+
+class QuitButtonEx extends JFrame {
+
+    public QuitButtonEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        JButton quitButton = new JButton("Quit");
+
+        quitButton.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        createLayout(quitButton);
+
+        setTitle("Quit button");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
 }
