fa453e8887cc00a3eb6677f158aff494a477c4a7
Trevor Svec <tsvec5678@gmail.com>
2016-12-21 22:06:31 -0500

    update-trevor

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//testing edits
+		//testing update from local eclipse edit - Trevor
+		System.out.println("hello world");
 	}
 
 }
